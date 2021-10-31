3d30e854f93627b11f023fb9163d2c49f505bf3b
Le Duong Anh Chi <17521293@gm.uit.edu.vn>
2021-10-31 10:19:50 +0700

    syno

---
+++ views/index.js
@@ -0,0 +1,12 @@
+
+<html>
+    <head>
+    <title>Dien toan dam may</title>
+    
+    </head>
+    <body>
+    <h1>Le Duong Anh Chi</h1>
+    <h1> Do Cong Chi </h1>
+    <h1> Duong Minh Thanh</h1>
+    </body>
+    </html>
