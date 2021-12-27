af3d95153f188092ca6194e45a096c978bbf00b0
Le Duong Anh Chi <17521293@gm.uit.edu.vn>
2021-12-27 08:33:55 +0700

    fix mongoose

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const app = express()
-
+const mongoose = require("mongoose")
 var port = process.env.PORT || 8080
 
 app.use(express.static(__dirname + '/public'));
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.set("views", "./views");
+app.
 // create application/json parser
+//mongoose
+mongoose.connect('mongodb://localhost/dientoandammay')
+
 
  
 // create application/x-www-form-urlencoded parser
+console.log('con ga be be no dau canh tre 123')
 
 
+console.log('chan vlk ')
 console.log('con heo be be')
 
 
